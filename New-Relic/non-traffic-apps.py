20118ff9ffe888acca2e2590aecccfe361f72529
Anurag Sharma <anuragsharma@Anurag-Sharma-DevOps.local>
2020-01-31 12:52:05 +0530

    Adding Script

---
+++ New-Relic/non-traffic-apps.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import sys
+import os
+
+nr_url = "https://api.newrelic.com/v2/applications.json"
+
+res = requests.get(nr_url, headers={"X-Api-Key":"New-Relic-Account-API-Key"})
+app_json = json.loads(res.content)
+
+i = 0
+for task in app_json["applications"][:-1]:
+    if task["application_summary"]["throughput"] == 0:
+        print task["name"] , task["id"]
+    i += 1
