820a26f47a87361c8e1d04f5e38c1c67c897632a
Anurag Sharma <anuragsharma@Anurag-Sharma-DevOps.local>
2020-01-31 12:42:24 +0530

    Adding Script

--- scripts/non-reporting-apps-.py
+++ scripts/non-reporting-apps-.py
@@ -12,6 +12,6 @@ app_json = json.loads(res.content)
 
 i = 0
 for task in app_json["applications"][:-1]:
-    if task["health_status"] == "unknown":
-        print task["name"]
+    if task["reporting"] == 0:
+            print task["name"], task["id"]
     i += 1
